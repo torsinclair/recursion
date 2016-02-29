b9df6d933410dcb7b11c5851e7633abc4166322b
torsinclair <tor@romanesco.se>
2016-02-29 11:34:57 -0800

    Complete stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,27 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if( Array.isArray(obj) ){
+    var results = [];
+    for( var i = 0; i < obj.length; i++ ){
+      results.push( stringifyJSON(obj[i]) );
+    }
+    return '[' + results.join(',') + ']';
+  }
+
+  if( obj && typeof obj === 'object' ){
+    var results = [];
+    for( var keys in obj ){
+      if( obj[keys] === undefined || typeof obj[keys] === 'function' ){
+        continue;
+      }
+      results.push(stringifyJSON(keys) + ':' + stringifyJSON(obj[keys]));
+    }
+    return '{' + results.join(',') + '}';
+  }
+  if( typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  return '' + obj;
+
 };
